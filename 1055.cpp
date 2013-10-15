8c2506297b634190d30ea37068c6c4a1594b917d
ww <ljq258@gmail.com>
2013-10-16 00:47:57 +0800

    a

--- 1055.cpp
+++ 1055.cpp
@@ -1,49 +1,61 @@
-#include <cstdio>
-#include <vector>
-#include <cstring>
+#include<iostream>
+#include<cstring>
+#include<cstdio>
 #include <algorithm>
-#include <iostream>
 using namespace std;
-const int N=100005;
-int amin,amax
-struct Person{
-	char name[9];
-	int age;
-	int worth;
-}per[N];
-bool range(int age){
-	if(age<=amax&&age>=amin)
-		return true;
-	else
-		return false;
-}
-bool cmp(struct Person p1, struct Person p2){
-	//if(range(p1.age)!=range(p2.age))
-	//	return range(p1.age) < range(p2.age);
-	//if((p1.age<=amax&&p1.age>=amin)!=(p2.age<=amax&&p2.age>=amin))
-	//	return (p1.age<=amax&&p1.age>=amin) >  (p2.age<=amax&&p2.age>=amin);
-	//else if(p1.worth!=p2.worth)
-	//	return p1.worth > p2.worth;
-	//else if(p1.age!=p2.age)
-	//	return p1.age < p2.age;
-	//else 
-	//	return ;
-		//return strcmp(p1.name,p2.name) < 0;
-	return p1.age < p2.age;
-}
-int main(){
-	int n,k;
-	scanf("%d",&n);
-	for(int i=0;i<n;i++)
-		scanf("%s%d%d",per[i].name,&per[i].age,&per[i].worth);
-	for(int i=1;i<=k;i++){
-		int m;//[amin,amax]
-		scanf("%d%d%d",&m,&amin,&amax);
-		printf("Case #%d:\n",i);
-		sort(per,per+n,cmp);
-		//sort(per,per+n,cmp);
+struct node{
+	char name[10];
+	int age,worth;
+}a[100005];
+int b[20005];
+int c[205];
+bool cmp(const node& p,const node& q){
+	if(p.worth==q.worth)
+	{
+		if (p.age==q.age)
+		{
+			return strcmp(p.name,q.name)<=0;
+		}
+		return p.age<q.age;
 	}
-
-	system("PAUSE");
+	return p.worth>q.worth;
+}
+int main()
+{
+	int n,q,i,M,ma,mb,txt=1,len;
+	while(~scanf("%d %d",&n,&q))
+	{
+		memset(c,0,sizeof(c));
+		for (i=0;i<n;++i){
+			scanf("%s%d%d",a[i].name,&a[i].age,&a[i].worth);
+		}
+		sort(a,a+n,cmp);
+		for (len=i=0;i<n;++i){
+			if(c[a[i].age]<=100)
+			{
+				b[len++]=i;
+				c[a[i].age]++;
+			}
+		}
+		while(q--){
+			scanf("%d%d%d",&M,&ma,&mb);
+			if(ma>mb){
+				ma^=mb;
+				mb^=ma;
+				ma^=mb;
+			}
+			printf("Case #%d:\n",txt++);
+			int cnt=0;
+			for (i=0;i<len&&cnt<M;++i){
+				if(a[b[i]].age>=ma&&a[b[i]].age<=mb){
+					printf("%s %d %d\n",a[b[i]].name,a[b[i]].age,a[b[i]].worth);
+					cnt++;
+				}
+			}
+			if(cnt==0){
+				printf("None\n");
+			}
+		}
+	}
 	return 0;
 }
