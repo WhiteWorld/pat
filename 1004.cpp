859090c5ed0e350673e153b4221ffd058cbd1223
liu bai <ljq258@gmail.com>
2014-02-13 13:48:42 +0800

    a

--- 1004.cpp
+++ 1004.cpp
@@ -1,71 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <vector>
+#include <cstring>
 #include <algorithm>
+#include <iostream>
+const int N = 100;
+using namespace std;
 
-struct Node{
-	//int id;//1~99
-	int father;
-	int child_num;
+typedef struct {
+	int pid;
 	int level;
-};
-struct Node node[100];
-int n,m;
-int maxlevel;
-void update(int id){
-	for(int i=2;i<100;i++)
-		if(node[i].father>0&&node[i].father==id){
-			node[i].level = node[id].level+1;
-			if(node[i].child_num>0)
-				update(i);
-			else{
-				if(node[i].level>maxlevel)
-					maxlevel = node[i].level;
-			}
-		}
+	bool have_child;
+}Node;
+Node node[N];
+
+
+int get_level(int id){
+	int pid=node[id].pid;
+	if(node[pid].level!=-1)
+		return node[pid].level+1;
+	else 
+		return get_level(pid)+1;
 }
-
-int main(){
-	maxlevel = -1;
-	node[1].level=0;
-	node[1].child_num=0;
-	node[1].father=-1;
-
+int main()
+{
+	int n,m;
+	int l[N];//hash level
 	scanf("%d%d",&n,&m);
+	for(int i=1;i<N;i++){
+		node[i].level=-1;
+		node[i].have_child=false;
+		l[i]=0;
+	}
+	node[1].pid=-1;//root
+	node[1].level=1;
 	for(int i=0;i<m;i++){
-		int id,k;
-		scanf("%d%d",&id,&k);
-		node[id].child_num=k;
-		for(int j=0;j<k;j++){
-			int tid;
-			scanf("%d",&tid);
-			node[tid].father = id;
+		char str[3];
+		int pid,num;
+		scanf("%s%d", str,&num);
+		pid=(str[0]-'0')*10+str[1]-'0';
+		node[pid].have_child=true;
+		while(num--){
+			scanf("%s",str);
+			int cid;
+			cid=(str[0]-'0')*10+str[1]-'0';
+			node[cid].pid=pid;
 		}
 	}
-	update(1);
-	if(n==1)
-		printf("1");
-	else
-		printf("0");
-	for(int i=1;i<=maxlevel;i++){
-		int sum=0;
-		for(int j=0;j<100;j++)
-			if(node[j].level==i&&node[j].child_num==0)
-				sum++;
-		printf(" %d",sum);
+	int maxlevel=-1;
+	for(int i=1;i<=n;i++){
+		if(node[i].level==-1)
+			node[i].level=get_level(i);
+		if(!node[i].have_child)
+			l[node[i].level]++;
+		if(node[i].level>maxlevel)
+			maxlevel=node[i].level;
 	}
-
-	/*
-	for(int i=2;i<100;i++)
-		if(node[i].father>0){
-
-		}
-		*/
-	//printf("%02d  %d",id,k);
-	system("PAUSE");
-	return 0;
+	printf("%d",l[1]);
+	for(int i=2;i<=maxlevel;i++)
+		printf(" %d",l[i]);
+	//system("Pause");
+    return 0;
 }
-/*
-update_level(int id)
-
-*/
